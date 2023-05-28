fcaab28e9f9321359dbeb22056e57b6ab0ecbf39
Joosep <joosep.vorno@gmail.com>
2023-05-28 14:05:04 +0300

    django admin, item modules, uploads interface

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+CATEGORY_CHOICES = (
+	("F", "Garden"),
+	("S", "Varia"),
+	("M", "Home"),
+	("T", "Car"),
+	("R", "18+"),
+)
+
+class Item(models.Model):
+	title = models.CharField(max_length=100)
+	price = models.FloatField()
+	discount_price = models.FloatField()
+	category = models.CharField(choices=CATEGORY_CHOICES, max_length=5)
+	slug = models.SlugField()
+	description = models.TextField()
+	image = models.ImageField()
+
+	def __str__(self):
+		return self.title
