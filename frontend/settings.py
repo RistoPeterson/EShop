43b0396ffe99d0de99d3ec9c6259af281ec4dce6
Risto Peterson <risto.peterson@gmail.com>
2023-05-29 10:13:10 +0300

    Adding items and photos to the page

--- frontend/settings.py
+++ frontend/settings.py
@@ -119,13 +119,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/')
-]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 MEDIA_URL = '/uploads/'
 MEDIA_ROOT = (
-    os.path.join(BASE_DIR, "uploads")
+    os.path.join(BASE_DIR, 'uploads')
 )
 
 
