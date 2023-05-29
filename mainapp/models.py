89df510dd520d73a9085ef9139447a086909d502
Risto Peterson <risto.peterson@gmail.com>
2023-05-29 12:37:09 +0300

    Making home page to show products and product detailview

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.shortcuts import reverse
 
 # Create your models here.
 
@@ -14,7 +15,7 @@ CATEGORY_CHOICES = (
 class Item(models.Model):
     title = models.CharField(max_length=100)
     price = models.FloatField()
-    discount_price = models.FloatField()
+    discount_price = models.FloatField(blank=True)
     category = models.CharField(choices=CATEGORY_CHOICES, max_length=5)
     slug = models.SlugField()
     description = models.TextField()
@@ -22,3 +23,14 @@ class Item(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_discount_percent(self):
+        discount_percent = 100 - (self.discount_price * 100 / self.price)
+        return discount_percent
+
+    def get_item_url(self):
+        return reverse('mainapp:detail', kwargs={
+            'slug': self.slug
+        })
+
+
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from .models import Item
+from django.views.generic import View, DetailView, ListView
 
 
 # Create your views here.
-def index(request):
-    context = {
-        'items': Item.objects.all()
-    }
-    return render(request, 'index.html', context=context)
+# def index(request):
+#    context = {
+#        'items': Item.objects.all()
+#    }
+#    return render(request, 'index.html', context=context)
+
+
+class IndexView(ListView):
+    model = Item
+    paginate_by = 10
+    template_name = 'index.html'
+
+
+class ProductDetailView(DetailView):
+    model = Item
+    template_name = 'product.html'
