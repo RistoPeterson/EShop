0fc2c850ed26830565a3353079ba0d9bd96440ff
Joosep <joosep.vorno@gmail.com>
2023-05-31 20:32:00 +0300

    made path in url for accounts

--- frontend/urls.py
+++ frontend/urls.py
@@ -24,7 +24,8 @@ from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('mainapp.urls'))
+    path('', include('mainapp.urls')),
+    path('accounts/', include('accounts.urls'))
 ]
 
 urlpatterns += staticfiles_urlpatterns()
